f0cc652a0a3fad0a66788a1fd6a5f032a7bd35e1
Dan Jacob <danjac@danjac-thinkpad.(none)>
2012-07-28 22:55:48 +0300

    Bootstrap files added

--- src/shoutouts/views/welcome.clj
+++ src/shoutouts/views/welcome.clj
@@ -1,9 +1,8 @@
 (ns shoutouts.views.welcome
-  (:require [shoutouts.views.common :as common]
-            [noir.content.getting-started])
+  (:require [shoutouts.views.common :as common])
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]))
 
-(defpage "/welcome" []
+(defpage "/" []
          (common/layout
            [:p "Welcome to shoutouts"]))
